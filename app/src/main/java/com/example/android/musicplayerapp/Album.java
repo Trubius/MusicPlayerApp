a48708892eb8312250bb4a1ab8d7cc053ef84e3d
Trubius <trubel.gabor@gmail.com>
2018-06-05 19:20:42 +0100

    Insert UUIDs on Albums and store them in a HashMap

--- app/src/main/java/com/example/android/musicplayerapp/Album.java
+++ app/src/main/java/com/example/android/musicplayerapp/Album.java
@@ -1,18 +1,23 @@
 package com.example.android.musicplayerapp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class Album extends ArrayList<Track> {
 
     private Artist mArtist;
     private String mAlbumName;
     private int mAlbumCoverId;
+    private String mKey = UUID.randomUUID().toString();
+    private static HashMap<String, Album> mAlbumsByKey = new HashMap<>();
 
     public Album(String artistName, String albumName, int coverId){
         mArtist = Artist.getOrCreateArtist(artistName);
         mArtist.addAlbum(this);
         mAlbumName = albumName;
         mAlbumCoverId = coverId;
+        mAlbumsByKey.put(mKey, this);
     }
 
     public String getArtistName() {
@@ -34,5 +39,12 @@ public class Album extends ArrayList<Track> {
     public void addTrack(Track track){
         add(track);
         track.setAlbum(this);
+    }
+    public String getKey(){
+        return mKey;
+    }
+
+    public static Album getAlbumByKey(String key){
+        return mAlbumsByKey.get(key);
     }
 }
